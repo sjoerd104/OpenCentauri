e920f62606916900c56368ea69f512d8b01d98b5
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2025-07-20 16:54:35 +0200

    Cargo fmt

--- wifi-network-config-tool/src/config.rs
+++ wifi-network-config-tool/src/config.rs
@@ -1,14 +1,17 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[command(name = "wifi-network-config-tool", about = "Utility to interact with elegoo's wireless configuration", version = "0.1")]
-pub struct Args 
-{
+#[command(
+    name = "wifi-network-config-tool",
+    about = "Utility to interact with elegoo's wireless configuration",
+    version = "0.1"
+)]
+pub struct Args {
     #[arg(required = true)]
-    pub config_path : String,
+    pub config_path: String,
 
     #[command(subcommand)]
-    pub action : Commands,
+    pub action: Commands,
 }
 
 #[derive(Subcommand, Debug)]
--- wifi-network-config-tool/src/main.rs
+++ wifi-network-config-tool/src/main.rs
@@ -1,6 +1,6 @@
 mod config;
-use config::Args;
 use clap::Parser;
+use config::Args;
 
 struct ConnectionInformation {
     ssid: String,
@@ -11,8 +11,10 @@ fn main() {
     let args = Args::parse();
     let bytes = std::fs::read(&args.config_path).expect("Failed to read configuration file");
 
-    let current_configs_count_1 = u32::from_le_bytes(bytes[0x00..0x04].try_into().unwrap()) as usize;
-    let current_configs_count_2 = u32::from_le_bytes(bytes[0x04..0x08].try_into().unwrap()) as usize;
+    let current_configs_count_1 =
+        u32::from_le_bytes(bytes[0x00..0x04].try_into().unwrap()) as usize;
+    let current_configs_count_2 =
+        u32::from_le_bytes(bytes[0x04..0x08].try_into().unwrap()) as usize;
     let max_configs_count = u32::from_le_bytes(bytes[0x08..0x0C].try_into().unwrap());
     let bytes_per_config = u32::from_le_bytes(bytes[0x0C..0x10].try_into().unwrap());
 
@@ -24,20 +26,33 @@ fn main() {
     let chunks = bytes[0x20..].chunks_exact(bytes_per_config as usize);
     let mut configs = Vec::new();
 
-    chunks.into_iter().take(current_configs_count_1).for_each(|f| {
-        configs.push(ConnectionInformation {
-            ssid: String::from_utf8_lossy(&f[0x00..0x21]).trim().to_string(),
-            password: String::from_utf8_lossy(&f[0x21..]).trim().to_string(),
+    chunks
+        .into_iter()
+        .take(current_configs_count_1)
+        .for_each(|f| {
+            configs.push(ConnectionInformation {
+                ssid: String::from_utf8_lossy(&f[0x00..0x21]).trim().to_string(),
+                password: String::from_utf8_lossy(&f[0x21..]).trim().to_string(),
+            });
         });
-    });
 
     configs.reverse();
 
     match args.action {
         config::Commands::List => {
-            println!("Stored configurations: {}/{}", current_configs_count_1, max_configs_count);
+            println!(
+                "Stored configurations: {}/{}",
+                current_configs_count_1, max_configs_count
+            );
             println!("Bytes per configuration: {}", bytes_per_config);
-            println!("Checksum: {}", checksum.iter().map(|b| format!("{:02X}", b)).collect::<Vec<String>>().join(""));
+            println!(
+                "Checksum: {}",
+                checksum
+                    .iter()
+                    .map(|b| format!("{:02X}", b))
+                    .collect::<Vec<String>>()
+                    .join("")
+            );
             println!();
 
             let mut last_used = true;
